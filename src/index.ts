94eaeb13f8dfdbc5fc410bceb7b85ebc1185b583
Nikita Skovoroda <chalkerx@gmail.com>
2025-06-05 23:02:21 +0400

    restore import order for matching bundle

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 // Copyright © Aptos Foundation
 // SPDX-License-Identifier: Apache-2.0
 
+// Matching import order (to old ./accounts) minimizes bundle diff
+import "./utils";
+import "./generated/index";
+import "./aptos_types";
+import "./bcs";
+
 export * as BCS from "./bcs";
 export * from "./transaction_builder";
 export * as TokenTypes from "./aptos_types/token_types";
